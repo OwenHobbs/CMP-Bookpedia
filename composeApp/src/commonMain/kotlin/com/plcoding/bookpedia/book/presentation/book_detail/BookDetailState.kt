0b6712612f3045351e6a47769688840286c6998f
Owen Hobbs <owenhobbs2006@gmail.com>
2025-12-27 23:46:08 -0600

    Finished BookDetailScreen

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailState.kt
@@ -4,6 +4,6 @@ import com.plcoding.bookpedia.book.domain.Book
 
 data class BookDetailState(
     val isLoading: Boolean = true,
-    val isFavorite: Boolean = true,
+    val isFavorite: Boolean = false,
     val book: Book? = null
 )
