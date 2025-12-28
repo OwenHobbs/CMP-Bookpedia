591725f4725867060bd9e2a345077e3ebcf04ea7
Owen Hobbs <owenhobbs2006@gmail.com>
2025-12-28 00:34:10 -0600

    Fetch book descriptions

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.data.safeCall
@@ -20,7 +21,7 @@ class KtorRemoteBookDataSource(
         resultLimt: Int?
     ): Result<SearchResponseDto, DataError.Remote> {
         // TODO: how does this give a SearchResponseDto?
-        return safeCall {
+        return safeCall<SearchResponseDto> {
             httpClient.get(
                 urlString = "$BASE_URL/search.json"
             ) {
@@ -29,6 +30,14 @@ class KtorRemoteBookDataSource(
                 parameter("language", BOOK_LANGUAGE)
                 parameter("fields", "key,title,author_name,author_key,cover_edition_key,cover_i,ratings_average,ratings_count,first_publish_year,language,number_of_pages_median,edition_count")
             }
+        }
+    }
+
+    override suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote> {
+        return safeCall<BookWorkDto> {
+            httpClient.get(
+                urlString = "$BASE_URL/works/$bookWorkId.json"
+            )
         }
     }
 }
